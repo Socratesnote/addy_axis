6b24270b9bae4bcfef3a027dcf4b2643d144654a
Thomas P <tp@u.northwestern.edu>
2023-07-14 11:17:52 -0500

    Merging changes from development.

--- addy_axis.m
+++ addy_axis.m
@@ -4,12 +4,10 @@ function varargout = addy_axis(varargin)
 %cursor functionality and scaling.
 
 % Use:
-% axes_struct = addy_axis;
-% axes_struct = addy_axis('ax', 'gca');
-% axes_struct = addy_axis('side', 'left');
-% axes_struct = addy_axis('ax', 'gca', 'side', 'right');
-% axes_struct = addy_axis(____, 'offset', 0.03);
-% axes_struct = addy_axis(____, 'hold', 'on')
+% axes_struct = addyaxis('ax', 'gca');
+% axes_struct = addyaxis('side', 'left');
+% axes_struct = addyaxis('ax', 'gca', 'side', 'right');
+
 % Inspired by yyaxis,
 % plotyyy (https://www.mathworks.com/matlabcentral/fileexchange/1017-plotyyy),
 % and addaxis (https://www.mathworks.com/matlabcentral/fileexchange/9016-addaxis).
@@ -32,15 +30,15 @@ function_parser.KeepUnmatched = true;
 % end
 
 % Optional
-defaultOptional = {gca, 'right'};
+defaultOptional = {gca, ''};
 optionalArguments = {'ax', 'side'};
 for ii = 1:length(optionalArguments)
   addOptional(function_parser, optionalArguments{ii}, defaultOptional{ii})
 end
 
 % Parameters
-defaultParameter = {0.04, 'off'};
-parameterArguments = {'offset', 'hold'};
+defaultParameter = {0.04};
+parameterArguments = {'offset'};
 for ii = 1:length(parameterArguments)
   addParameter(function_parser, parameterArguments{ii}, defaultParameter{ii})
 end
@@ -52,42 +50,28 @@ parse(function_parser, varargin{:});
 axes_offset = function_parser.Results.offset;
 main_axes = function_parser.Results.ax;
 side = function_parser.Results.side;
-do_hold = function_parser.Results.hold;
 
 %% AddYAxis
 % First create faux axes that will be visible, but are not used for
 % plotting. Depending on how many axes already exist (or user
 % arguments), plot on the right or the left.
 
-hparent = main_axes.Parent;
-
-% Count number of added axes, but only to the 'tile' were working on.
-% This needs some extension of Matlab's lackluster implementation.
-% addy_axis does not work well with subplot(X), but can synergize with
-% tiledlayoutplus.
-if strcmpi(class(hparent), 'matlab.graphics.layout.TiledChartLayout')
-  htile = hparent;
-  current_tile = getappdata(htile, 'CurrentTile');
-  hax = nexttile(htile, current_tile);
-elseif strcmpi(class(hparent), 'matlab.ui.Figure') % Simple figure, no tiles
-  hax = main_axes;
-  current_tile = 0;
-  %   for ii = 1:(length(hfig.Children) - 1) % Assuming at least 1 axes child already exists
-  %     if ~strcmpi(class(hfig.Children(ii)), 'matlab.graphics.axis.Axes')
-  %       continue
-  %     end
-  %     num_addy_axes = num_addy_axes + 0.5;
-  %   end
-end
-num_addy_axes = getappdata(hax, 'AddedYAxes');
-
-if isempty(num_addy_axes)
-  num_addy_axes = 0;
+% Count number of added axes
+hfig = main_axes.Parent;
+num_addy_axes = 1; % Including the one we're about to create
+for ii = 1:(length(hfig.Children) - 1) % Assuming at least 1 axes child already exists
+    if ~strcmpi(class(hfig.Children(ii)), 'matlab.graphics.axis.Axes')
+      continue
+    end
+    num_addy_axes = num_addy_axes + 0.5;
 end
 
 % Determine side
-if mod(num_addy_axes, 2) == 0
+if isempty(side)
   side = 'right';
+  if mod(num_addy_axes, 2) == 0
+    side = 'left';
+  end
 end
 
 % Create new visible axes
@@ -104,7 +88,6 @@ else
 end
 axes_pair.axes_visible.Position(3) = 0; % Flatten the x-axis of the visible axes
 axes_pair.axes_visible.Toolbar.Visible = 'off'; % Hide the toolbar
-setappdata(axes_pair.axes_visible, 'CurrentTile', current_tile);
 
 % Create a new axes object to hold the plotted data. This will not be
 % visible or interactable.
@@ -112,10 +95,6 @@ axes_pair.axes_hidden = axes('Position', main_axes.Position);
 axes_pair.axes_hidden.Visible = 'off';
 axes_pair.axes_hidden.HitTest = 'off';
 axes_pair.axes_hidden.Toolbar.Visible = 'off';
-if strcmpi(do_hold, 'on')
-  axes_pair.axes_hidden.NextPlot = 'add';
-end
-setappdata(axes_pair.axes_hidden, 'CurrentTile', current_tile);
 
 % Set callbacks for home, zoom and pan to update all axes when the
 % limits of the main axes change. Click-and-drag is disabled.
@@ -138,10 +117,6 @@ set(hpan,'ActionPostCallback', @addy_pan_post);
 % detect button release.
 set(main_axes, 'ButtonDownFcn',@addy_click_pre);
 set(gcf,'WindowButtonUpFcn',@addy_click_post);
-
-% Increment number of added axes
-num_addy_axes = num_addy_axes + 1;
-setappdata(hax, 'AddedYAxes', num_addy_axes);
 
 %% Outputs
 varargout = {axes_pair};
